52926a42006ed8d95022c8608b9c3432b84cc590
Thato <thatojakemoema@gmail.com>
2023-07-21 10:00:10 +0200

    Completed 5 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...theArgs) {
 
+    for (let i = 0; i < theArgs.length; i++)
+    {
+        for (let j = 0; j < list.length; j++)
+
+        if(list[j] === theArgs[i])
+        {
+            list.splice(j, 1);
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+    let middleNum;
+   
+    if(num2 < num1)
+    {
+        middleNum = num2;
+        num2 = num1;
+        num1 = middleNum
+    }
+    if(num1 >= 0 && num2 >= 0 && typeof num1 === "number" && typeof num2 === "number")
+    {    
+        while (num1 <= num2 )
+        {
+            sum += num1;
+
+            num1++;
+        }
+        return sum;
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
