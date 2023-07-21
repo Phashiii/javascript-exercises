e008f39fb01ec1eaabc119717913454dd7ed8995
Thato <thatojakemoema@gmail.com>
2023-07-21 08:12:57 +0200

    First two tests completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+   
 
+    let output = "";
+    if(count < 0)
+    {
+        output = "ERROR";
+        return output;
+    }else
+    {
+        while(count> 0)
+        {
+            output += word;
+
+            count--;
+        }
+        return output;
+    }  
+    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,32 @@
-const reverseString = function() {
+const reverseString = function(stringtoReverse) {
 
+    let counter = 0;
+    let counter2 = 0;
+    let characters = [];
+    let reversedCharacters = [];
+    let reversedString = "";
+
+    while(counter < stringtoReverse.length)
+    {
+        characters[counter] = stringtoReverse.charAt(counter);
+
+        counter ++;
+    }
+
+    let index = characters.length -1;
+    while(counter2 < characters.length)
+    {
+        reversedCharacters[counter2] = characters[index];
+
+        index--;
+        counter2++;
+    }
+
+    for(const chars of reversedCharacters){
+        reversedString += chars;
+    }
+
+    return reversedString
 };
 
 // Do not edit below this line
