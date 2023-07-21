f0be29406e4d0d919575af9a59a4b66e16c65bfd
Thato <thatojakemoema@gmail.com>
2023-07-21 10:57:04 +0200

    Completed tests until 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  converted = (temp -32) * (5/9);
+  if(Number.isInteger(converted))
+  {
+    return converted;
+  }else{
+    return Math.round(converted * 10) / 10;
+  }
+  return Math.round(converted, 2);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  converted = temp * (9/5) + 32;
+  if(Number.isInteger(converted))
+  {
+    return converted;
+  }else{
+    return Math.round(converted * 10) / 10;
+  }
+  return Math.round(converted, 2);
 };
 
 // Do not edit below this line
